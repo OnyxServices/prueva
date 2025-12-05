34dff78667814eb1f741c63ba5d00c57b4140824
Oto <djotoniel59@gmail.com>
2025-12-05 18:38:45 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js - Archivo centralizado de configuración
-export const SUPABASE_CONFIG = {
-  url: "https://okasjgfuqftbiqusyrpi.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rYXNqZ2Z1cWZ0YmlxdXN5cnBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NjU4MTcsImV4cCI6MjA4MDQ0MTgxN30.ROtN57SxMj44UFntnMgqwzFSG0PVL_nybaCA2lFAHrk"
-};
